1a95476de419d854a8b485f2920ee5d4cd26795a
SumitMakwana <sumit.mak674@gmail.com>
2022-08-05 19:18:10 +0530

    Email conformation stage 1 commit

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
 from flask import Flask, redirect, url_for, request, session, render_template
 import sqlite3
+from flask_mail import Mail, Message
+from itsdangerous import URLSafeTimedSerializer, SignatureExpired
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '123'
+
+app.config['MAIL_SERVER']='smtp.mailtrap.io'
+app.config['MAIL_PORT'] = 2525
+app.config['MAIL_USERNAME'] = '930864c7dc75f4'
+app.config['MAIL_PASSWORD'] = '1f89bec013cbfd'
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+
+mail = Mail(app)
+
+s = URLSafeTimedSerializer('123')
 
 @app.route('/')
 @app.route('/home')
@@ -17,6 +30,34 @@ def register():
 def login():
     return render_template('login.html', title = 'Login')
     
+@app.route('/cemail')
+def cemail():
+    return render_template('cemail.html', title = 'Confirm email')
+    
+@app.route('/confirm_email', methods=['POST'])
+def confirm_email():
+    if request.method == 'POST':
+        email = request.form['email']
+        token = s.dumps(email, salt = 'email-confirm')
+
+        msg = Message('Confirm email', sender = 'sumitmakwanaa@gmail.com', recipients = [email])
+
+        link = url_for('home', token = token, _external = True)
+
+        msg.body = 'your link is {}'.format(link)
+
+        mail.send(msg)
+
+        return render_template('afcdcemail.html', email = email, token = token, title = 'Confirm-email')
+
+@app.route('/confirmemail/<token>')
+def confirmemail(token):
+    try:
+        email = s.loads(token, salt = 'email-confirm', max_age = 500)
+    except SignatureExpired:
+        return '<h1>The token is expired</h1>'
+    return render_template('home.html')
+
 @app.route('/insertdata', methods=['POST'])
 def insertdata():
     if (request.method == 'POST'):
